45de3ff896eb8d364f7cfe8d99a1ae3ec3e3f10e
Chujia Guo <herobrinevinyl@gmail.com>
2025-08-10 22:28:32 +0800

    Added basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
     output: "export",
-  /* config options here */
+    assetPrefix: isProd ? '/webpage/' : '',
+    basePath: isProd ? '/webpage' : '',
+    /* config options here */
 };
 
 export default nextConfig;
