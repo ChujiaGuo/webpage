ad642aa34be8a59d4016518b6f685d3f98d717bc
Chujia Guo <herobrinevinyl@gmail.com>
2025-08-21 17:49:08 +0800

    Edited Config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
     output: "export",
-    assetPrefix: isProd ? '/webpage/' : '',
-    basePath: isProd ? '/webpage' : '',
     /* config options here */
 };
 
